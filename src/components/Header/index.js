4f818fbfa4f9ccd8c20010deda4bf596188ac6dc
Matheus Chaves <matheusca@hotmail.com>
2023-07-04 12:03:54 -0300

    Primeira Section finalizada. Fontes add no projeto e estilização do header completa

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,11 +3,11 @@ import './styles.css'
 export default function Header() {
     return (
         <section className='header-container'>
-            <p>Webinars exclusivos</p>
-            <h3>Menos conversinha,</h3>
-            <h1>Mais conversão</h1>
+            <p className='first-p'>Webinars exclusivos</p>
+            <h3>Menos Conversinha,</h3>
+            <h1>Mais Conversão</h1>
             <hr></hr>
-            <p>Conheça estratégias que <strong>mudaram o jogo</strong> e como aplicá-las no seu negócio</p>
+            <p className='second-p'>Conheça estratégias que <strong>mudaram o jogo</strong> e como aplicá-las no seu negócio</p>
         </section>
     );
 };
