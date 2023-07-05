4d9b9d8fce51c0c29e378426f782ace6fa23d1dc
Matheus Chaves <matheusca@hotmail.com>
2023-07-05 19:38:03 -0300

    Botões da Section 2 funcionando como devem

--- src/components/Section2/index.js
+++ src/components/Section2/index.js
@@ -1,16 +1,30 @@
+'use client'
+
 import Thumbnail from '../Thumbnail';
+import { useState } from 'react';
 import './styles.css';
 
 export default function Section2() {
+
+    const [ fakeData, setFakeData] = useState(['item1', 'item2', 'item3']);
+
+    function getFakeData() {
+       const fakeDataFromDB = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9'];
+       setTimeout(() =>{
+        setFakeData(fakeDataFromDB)
+       }, 1000)    
+    }
+    
+    
     return(
         <section className='section2'>
             <div className='btns-container'>
                 <div className='generic-btns'>
-                    <button className='btn1'>Agências</button>
-                    <button className='btn2'>Chatbot</button>
-                    <button className='btn3'>Marketing Digital</button>
-                    <button className='btn4'>Geração de Leads</button>
-                    <button className='btn5'>Mídia Paga</button>
+                    <button onClick={getFakeData} className='btn1'>Agências</button>
+                    <button onClick={getFakeData} className='btn2'>Chatbot</button>
+                    <button onClick={getFakeData} className='btn3'>Marketing Digital</button>
+                    <button onClick={getFakeData} className='btn4'>Geração de Leads</button>
+                    <button onClick={getFakeData} className='btn5'>Mídia Paga</button>
                 </div>
                 <div className='filter-btn'>
                   Ordenar por: <button className='btn6'>Data da publicação </button>
@@ -18,15 +32,11 @@ export default function Section2() {
             </div>
             <hr className='breakline-sec2'></hr>
             <div className='thumbnails'>
-                <Thumbnail />
-                <Thumbnail />
-                <Thumbnail />
-                <Thumbnail />
-                <Thumbnail />
-                <Thumbnail />
-                <Thumbnail />
-                <Thumbnail />
-                <Thumbnail />
+                {fakeData.map(item =>{
+                    return(
+                        <Thumbnail />
+                    )
+                })}
             </div>
             <hr className='breakline-sec2'></hr>
         </section>
--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -5,7 +5,7 @@ import Thumb from '../../assets/images/thumbnail.png';
 export default function Thumbnail() {
     return(
         <div className='thumbnail'>
-            <Image className='thumb-img' src={Thumb} />
+            <Image alt='marketing em 2014' className='thumb-img' src={Thumb} />
             <p className='thumb-text'>Como aumentar sua Geração de Leads feat Traktor </p>
         </div>
     );
